105c6453c77592dfee20527bd21d54271dfccc2a
sindhuja sesham <sindhuja.sesham13@gmail.com>
2025-10-26 08:34:26 -0400

    updating index to convert

--- Python/converToCoor.py
+++ Python/converToCoor.py
@@ -3,31 +3,30 @@ import re
 import json
 
 
-
-
-#
+#open file
 with open('data/final_flyable_routes.json') as file:
     data = json.load(file)
-# 
+
+# storing data into trips
 trips = data["flyable_trips"]
 arr_Num_Count = len(trips)
 
 all_trips = []
 for trip in trips:
-    coords = [[lon, lat] for _, lon, lat in trip ["flyable_path_gps"]]
+    # storing index, lon, lat
+    coords = [[idx, lon, lat] for idx, lon, lat in trip["flyable_path_gps"]]
     all_trips.append(coords)
 
-
+# Print to console
 for trip_idx, trip in enumerate(all_trips):
     print(f"\nTrip {trip_idx}:")
-    for lon, lat in trip:
-        print(f" {lon:.6f}, {lat:.6f}")
+    for idx, lon, lat in trip:
+        print(f" {idx}, {lon:.6f}, {lat:.6f}")
 
+# Write to file
 with open("trips_output.txt", "w") as f:
     for trip_idx, trip in enumerate(all_trips):
         f.write(f"Trip {trip_idx}:\n")
-        for lon, lat in trip:
-            f.write(f"  Lon: {lon:.6f}, Lat: {lat:.6f}\n")
+        for idx, lon, lat in trip:
+            f.write(f"  {idx}, Lon: {lon:.6f}, Lat: {lat:.6f}\n")
         f.write("\n")
-
-
